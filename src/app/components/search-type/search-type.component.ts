721ac6c6b15ea302ce05de577a763b244d4850b4
Ryan Arpon <ryan.arpon@rivtechglobal.com>
2026-01-12 22:57:15 +0800

    update footer style

--- src/app/components/search-type/search-type.component.ts
+++ src/app/components/search-type/search-type.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { BaseComponent } from 'src/app/base.component';
 
 @Component({
   selector: 'search-type',
   templateUrl: './search-type.component.html',
   styleUrls: ['./search-type.component.css']
 })
-export class SearchTypeComponent {
+export class SearchTypeComponent extends BaseComponent {
 
 }
