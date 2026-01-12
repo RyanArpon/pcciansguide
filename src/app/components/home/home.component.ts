3439aff08c1a79ca14a4e9fdbb5c36cc81cff406
Ryan Arpon <ryan.arpon@rivtechglobal.com>
2026-01-12 23:04:53 +0800

    update footer styling

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { BaseComponent } from 'src/app/base.component';
 
 @Component({
   selector: 'home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent extends BaseComponent {
 
 }
